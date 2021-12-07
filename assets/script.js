9e7e8613a00b15d3c56d185246dab7c954594456
sanix-darker <s4nixd@gmail.com>
2021-12-08 00:12:53 +0100

    feat: initial commit

---
+++ assets/script.js
@@ -0,0 +1,58 @@
+var demo = new Moovie({
+    selector: "#tvnjor",
+    icons: {
+         path: "https://raw.githubusercontent.com/BMSVieira/moovie.js/main/icons/"
+    }
+});
+
+var video = demo.video;
+var hls = new Hls();
+
+const play = (name, url) => {
+    document.getElementById('load_it').style.display = "block";
+    document.getElementById('ch_title').innerHTML = name;
+    hls.loadSource(url);
+    hls.attachMedia(video);
+    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+        document.getElementById('load_it').style.display = "none";
+        document.getElementById('ch_input').value = "";
+        document.getElementById('ch_input').dispatchEvent(new Event("keyup"));
+    });
+    demo.togglePlay();
+}
+
+if (Hls.isSupported()) {
+    play('France 24 Français (1080p)','https://static.france24.com/live/F24_FR_HI_HLS/live_tv.m3u8');
+}else{
+    alert('Navigator not supported !')
+}
+
+var input = document.getElementById('ch_input');
+input.onkeyup = function () {
+    var filter = input.value.toUpperCase();
+    var lis = document.getElementsByTagName('li');
+    for (var i = 0; i < lis.length; i++) {
+        var name = lis[i].textContent;
+        if (name.toUpperCase().indexOf(filter) == 0) 
+            lis[i].style.display = 'list-item';
+        else
+            lis[i].style.display = 'none';
+    }
+}
+
+fetch('/channels/fra.json')
+.then(response => {
+    document.getElementById('channel_list').innerHTML = ""
+    if (!response.ok) {
+        throw new Error("HTTP error " + response.status);
+    }
+    return response.json();
+})
+.then(json => {
+    json.map(elt => {
+        document.getElementById('channel_list').innerHTML += "<li class='lk' onclick=\"play(\'" + elt["name"] + "\',\'" + elt["url"] + "\')\">" + elt["name"] + "</li>";
+    });
+})
+.catch(function () {
+    this.dataError = true;
+});
---
+++ parser.py
@@ -0,0 +1,33 @@
+from m3u_parser import M3uParser
+import json
+import sys
+
+
+def parser_channels():
+    chns = ['eng', 'fra']
+    chans = []
+    for c in chns:
+        useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
+        parser = M3uParser(timeout=50, useragent=useragent)
+        parser.parse_m3u(f"https://iptv-org.github.io/iptv/languages/{c}.m3u")
+        parser.filter_by('status', 'GOOD')
+        parserred_list = parser.get_list()
+        
+        print(len(parserred_list))
+        # We remove unecessary stuffs
+        for j in parserred_list:
+            del j["category"]
+            del j["tvg"]
+            del j["country"]
+            del j["logo"]
+            del j["language"]
+            del j["status"]
+
+        # We append to the output
+        chans += parserred_list
+
+    open("./public/channels/bin.json", "w+", 1).write(json.dumps(chans, separators=(',', ":")))
+
+
+if __name__ == "__main__":
+    parser_channels()
